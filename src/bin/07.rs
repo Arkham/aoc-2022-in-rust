695086719a30955aae61059e0a0a60a88a63d06b
Ju Liu <liuju86@gmail.com>
2022-12-07 18:59:59 +0000

    Use nom to parse input

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,14 +1,14 @@
 use std::collections::HashMap;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let commands: Vec<CommandWithOutput> = parse_input(input);
+    let (_, commands) = commands_parser(input).unwrap();
     let fs: FileSystem = build_filesystem(commands);
     let sizes: HashMap<Vec<&str>, u32> = calculate_sizes(fs);
     Some(sizes.values().filter(|value| **value < 100000).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let commands: Vec<CommandWithOutput> = parse_input(input);
+    let (_, commands) = commands_parser(input).unwrap();
     let fs: FileSystem = build_filesystem(commands);
     let sizes: HashMap<Vec<&str>, u32> = calculate_sizes(fs);
     let total: u32 = sizes[&vec!["/"]];
@@ -32,43 +32,61 @@ enum FsEntry<'a> {
 }
 
 #[derive(Debug, PartialEq)]
-enum CommandWithOutput<'a> {
+enum Command<'a> {
     Cd(Path<'a>),
     Ls(Vec<FsEntry<'a>>),
 }
 
-use crate::CommandWithOutput::*;
+use crate::Command::*;
 
-fn parse_input(input: &str) -> Vec<CommandWithOutput> {
-    input
-        .split("$ ")
-        .filter_map(|chunk| match chunk.split_once('\n') {
-            Some(("ls", output)) => Some(Ls(parse_fs_entries(output))),
-            Some((other, _)) => match other.split_once(' ') {
-                Some(("cd", "/")) => Some(Cd(Path::Root)),
-                Some(("cd", "..")) => Some(Cd(Path::BackOne)),
-                Some(("cd", other)) => Some(Cd(Path::Dir(other))),
-                _ => None,
-            },
-            _ => None,
-        })
-        .collect()
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit1, not_line_ending},
+    combinator::{map, opt},
+    multi::{many1, separated_list1},
+    sequence::{preceded, separated_pair, terminated},
+    IResult,
+};
+
+fn commands_parser(input: &str) -> IResult<&str, Vec<Command>> {
+    many1(terminated(alt((cd_parser, ls_parser)), opt(tag("\n"))))(input)
 }
 
-fn parse_fs_entries(input: &str) -> Vec<FsEntry> {
-    input
-        .lines()
-        .filter_map(|line| match line.split_once(' ') {
-            Some(("dir", dir_name)) => Some(FsEntry::Folder(dir_name)),
-            Some((size, file)) => Some(FsEntry::File(file, size.parse().unwrap())),
-            None => None,
-        })
-        .collect()
+fn cd_parser(input: &str) -> IResult<&str, Command> {
+    map(
+        preceded(
+            tag("$ cd "),
+            alt((
+                map(tag("/"), |_| Path::Root),
+                map(tag(".."), |_| Path::BackOne),
+                map(not_line_ending, Path::Dir),
+            )),
+        ),
+        Command::Cd,
+    )(input)
+}
+
+fn ls_parser(input: &str) -> IResult<&str, Command> {
+    map(
+        preceded(tag("$ ls\n"), separated_list1(tag("\n"), ls_entry_parser)),
+        Command::Ls,
+    )(input)
+}
+
+fn ls_entry_parser(input: &str) -> IResult<&str, FsEntry> {
+    alt((
+        map(preceded(tag("dir "), not_line_ending), FsEntry::Folder),
+        map(
+            separated_pair(digit1, tag(" "), not_line_ending),
+            |(file_size, file_name)| FsEntry::File(file_name, file_size.parse().unwrap()),
+        ),
+    ))(input)
 }
 
 type FileSystem<'a> = HashMap<Vec<&'a str>, Vec<FsEntry<'a>>>;
 
-fn build_filesystem(commands: Vec<CommandWithOutput>) -> FileSystem {
+fn build_filesystem(commands: Vec<Command>) -> FileSystem {
     let mut result = HashMap::new();
     let mut current: Vec<&str> = vec!["/"];
     for command in commands {
@@ -122,17 +140,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_input() {
-        let input = "$ cd /\n$ ls\ndir a\n123 g\n$ cd a\n$ cd ..\n";
+    fn test_parser() {
         assert_eq!(
-            parse_input(input),
-            vec![
-                Cd(Path::Root),
-                Ls(vec![FsEntry::Folder("a"), FsEntry::File("g", 123)]),
-                Cd(Path::Dir("a")),
-                Cd(Path::BackOne)
-            ]
-        );
+            commands_parser("$ ls\ndir a\n123 foo.txt\n$ cd /\n$ cd ..\n$ cd foo"),
+            Ok((
+                "",
+                vec![
+                    Command::Ls(vec![FsEntry::Folder("a"), FsEntry::File("foo.txt", 123)]),
+                    Command::Cd(Path::Root),
+                    Command::Cd(Path::BackOne),
+                    Command::Cd(Path::Dir("foo")),
+                ]
+            ))
+        )
     }
 
     #[test]
